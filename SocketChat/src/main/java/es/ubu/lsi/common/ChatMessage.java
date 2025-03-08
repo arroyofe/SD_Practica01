33c9c75374a73a60285de461fa42c30fc8883549
Fernando <far0006@alu.ubu.es>
2025-03-08 14:07:26 +0100

    Escritura de la clase CharMessage

---
+++ SocketChat/src/main/java/es/ubu/lsi/common/ChatMessage.java
@@ -0,0 +1,95 @@
+package es.ubu.lsi.common;
+
+import java.io.Serializable;
+
+/**
+ * Mensaje que aparece en el chat
+ * 
+ * @Autor Fernando Arroyo
+ */
+
+public class ChatMessage implements Serializable {
+
+	/**
+	 * UID por defecto
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public enum MessageType{
+		/*Mensaje*
+		MESSAGE,
+		/*Shutdom del servidor*/
+		SHUTDOWN,
+		/*Logout del Cliente*/
+		LOGOUT;
+	}
+	
+	// Tipo de mensaje
+	private MessageType tipo;
+	
+	// Texto del mensaje
+	private String mensaje;
+	
+	//Identificación del cliente
+	private int id;
+	
+	/*
+	 * Constructor
+	 * 
+	 * @return type
+	 * @see type
+	 */
+	public ChatMessage(int id, MessageType tipo, String mensaje) {
+		this.setId(id);
+		this.setTipo(tipo);
+		this.setMessage(mensaje);
+	}
+
+	/*
+	 * Obtiene el tipo
+	 * @return tipo
+	 */
+	public MessageType getTipo() {
+		return tipo;
+	}
+	
+	/*
+	 * Establece el tipo
+	 * @param tipo
+	 */
+	public void setTipo(MessageType tipo) {
+		this.tipo = tipo;
+	}
+	
+	/*
+	 * Obtiene el mensaje
+	 * @return mensaje
+	 */
+	public String getMessage() {
+		return mensaje;
+	}
+	
+	/*
+	 * Establece el mensaje
+	 * @param mensaje
+	 */
+	public void setMessage(String mensaje) {
+		this.mensaje = mensaje;
+	}
+	
+	/*
+	 * Obtiene la identificación
+	 * @return id
+	 */
+	public int getId() {
+		return id;
+	}
+	
+	/*
+	 * Establece la identificación
+	 * @param id
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}	
+}
